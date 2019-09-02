adf0b60e2b2cf19cb8c26698d7afb42b727977ce
Roman Lunev <r.v.lunev@gmail.com>
2019-09-02 17:35:52 +0300

    add coverpoint func

--- coverpoint/__init__.py
+++ coverpoint/__init__.py
@@ -16,8 +16,14 @@
 
 def coverpoint(segments):
     segments.sort(key=lambda s: s[1])
-
-    return segments
+    result = []
+    result.append(segments[0][1])
+    for s in segments:
+        l = s[0]
+        r = s[1]
+        if l > result[-1]:
+            result.append(r)
+    return result
 
 
 def main():
@@ -28,7 +34,9 @@ def main():
         segment = tuple(map(int, input().split()))
         segments.append(segment)
 
-    print(coverpoint(segments))
+    result = coverpoint(segments)
+    print(len(result))
+    print(" ".join(str(x) for x in result))
 
 
 if __name__ == "__main__":
---
+++ test/test_coverpoint.py
@@ -0,0 +1,15 @@
+import unittest
+from coverpoint import coverpoint
+
+
+class TestCoverPoint(unittest.TestCase):
+    def test_1(self):
+        segments = [(1, 3), (2, 5), (3, 6)]
+        self.assertEqual(coverpoint(segments), [3])
+
+    def test_2(self):
+        segments = [(4, 7), (1, 3), (2, 5), (5, 6)]
+        self.assertEqual(coverpoint(segments), [3, 6])
+
+if __name__ == '__main__':
+    unittest.main()
