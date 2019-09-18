7f5190f3ed05fdf716aee69b3c345fc5a62c9f1e
Roman Lunev <r.v.lunev@gmail.com>
2019-09-18 17:53:33 +0300

    walk_tree

--- huffman_code/__init__.py
+++ huffman_code/__init__.py
@@ -11,21 +11,32 @@
 from collections import Counter
 
 
-def huffman_code(s):
+def build_tree(s):
 
-    freq = Counter(s)
-    tree = OrderedDict()
-    temp = {}
+    freq = sorted(Counter(s).items(), key=lambda pair: pair[1], reverse=False)
 
-    while len(freq) > 1:
-        min_key_i = min(freq, key=freq.get)
-        F_i = freq.pop(min_key_i, None)
-        temp[min_key_i] = F_i
-        min_key_j = min(freq, key=freq.get)
-        F_j = freq.pop(min_key_j, None)
-        temp[min_key_j] = F_j
-        node = min_key_i + min_key_j
-        freq[node] = F_i + F_j
-        tree[node] = [min_key_i, min_key_j]
+    while len(freq) >= 2:
+        F_i = freq.pop(0)
+        F_j = freq.pop(0)
+        item = ((F_i[0], F_j[0]), F_i[1] + F_j[1])
+        freq.append(item)
+        freq = sorted(freq, key=lambda pair: pair[1], reverse=False)
 
-    return tree
+    return freq[0][0]
+
+
+def walk_tree(tree, current_code='', codes={}):
+
+    if type(tree[0]) == tuple:
+        walk_tree(tree[0], current_code + '0', codes)
+    else:
+        codes[tree[0]] = current_code + '0'
+    if type(tree[1]) == tuple:
+        walk_tree(tree[1], current_code + '1', codes)
+    else:
+        codes[tree[1]] = current_code + '1'
+
+    return(codes)
+
+tree = build_tree(s)
+huff = walk_tree(tree)
