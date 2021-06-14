57aa660fdb6e2fd8179e7a958586db0a36b22482
Roman Lunev <r.v.lunev@gmail.com>
2021-06-14 13:54:22 +0300

    accept changes

--- priority_queue/__init__.py
+++ priority_queue/__init__.py
@@ -1,35 +1,4 @@
 class PriorityQueue:
-<<<<<<< HEAD
-    def __init__(self):
-        self.array = []
-        self.heap_size = 0
-    
-
-    def _sift_up(self, index):
-        parent_node = index//2 - 1
-        if index == 1 or self.array[index-1] <= self.array[parent_node]:
-            return
-        self.array[index-1], self.array[parent_node] = self.array[parent_node], self.array[index-1]
-        self._sift_up(parent_node)
-
-
-    def _heapify(self, array, root):
-
-        heap_size = len(array[root:])
-        root += 1
-        left = root*2
-        right = root*2+1
-        largest = root
-
-        if left<=heap_size and array[left-1] > array[largest-1]:
-            largest = left
-        if right<=heap_size and array[right-1] > array[largest-1]:
-            largest = right
-        if largest != root:
-            array[root-1], array[largest-1] = array[largest-1], array[root-1]
-            self._heapify(array, largest-1)
-        
-=======
     """Implementation of priority queue"""
 
     def __init__(self):
@@ -60,29 +29,15 @@ class PriorityQueue:
             array[root], array[largest] = array[largest], array[root]
             self._sift_down(array, largest)
         return
->>>>>>> db73178322e3ad0ff49bd5c47a241994ecc0a1d5
 
     def insert(self, element):
         self.array.append(element)
         self.heap_size += 1
         self._sift_up(self.heap_size)
-<<<<<<< HEAD
-=======
-
->>>>>>> db73178322e3ad0ff49bd5c47a241994ecc0a1d5
         return
 
     def extract_max(self):
         max_element = self.array[0]
-<<<<<<< HEAD
-        self.array[0] = self.array.pop(self.heap_size-1)
-        self.heap_size -= 1
-        self._heapify(self.array, 0)
-        return max_element
-
-if __name__ == "__main__":
-    
-=======
         self.heap_size -= 1
         if self.heap_size < 1:
             self.array = []
@@ -94,7 +49,6 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
 
->>>>>>> db73178322e3ad0ff49bd5c47a241994ecc0a1d5
     n = int(input())
 
     a = PriorityQueue()
@@ -102,14 +56,7 @@ if __name__ == "__main__":
     for i in range(n):
         try:
             action, element = input().split(" ")
-<<<<<<< HEAD
-            a.insert(element)
-        except ValueError:
-            max_element = a.extract_max()
-            print(max_element)
-=======
             a.insert(int(element))
         except ValueError:
             max_element = a.extract_max()
             print(max_element)
->>>>>>> db73178322e3ad0ff49bd5c47a241994ecc0a1d5
--- test/test_priority_queue.py
+++ test/test_priority_queue.py
@@ -1,45 +1,10 @@
 import unittest
-<<<<<<< HEAD
-=======
 from random import randint
->>>>>>> db73178322e3ad0ff49bd5c47a241994ecc0a1d5
 from priority_queue import PriorityQueue
 
 
 class TestPriorityQueue(unittest.TestCase):
     def test_1(self):
-<<<<<<< HEAD
-        a = PriorityQueue()
-        a.insert(200)
-        a.insert(10)
-        self.assertEqual(a.array, [200, 10])
-
-    def test_2(self):
-        a = PriorityQueue()
-        a.insert(200)
-        a.insert(10)
-        heap_max = a.extract_max()
-        self.assertEqual(heap_max, 200)
-
-    def test_3(self):
-        a = PriorityQueue()
-        a.insert(200)
-        a.insert(10)
-        a.extract_max()
-        a.insert(5)
-        a.insert(500)
-        self.assertEqual(a.array, [500, 5, 10])
-    
-    def test_4(self):
-        a = PriorityQueue()
-        a.insert(200)
-        a.insert(10)
-        a.extract_max()
-        a.insert(5)
-        a.insert(500)
-        heap_max = a.extract_max()
-        self.assertEqual(heap_max, 500)
-=======
         queue = PriorityQueue()
         queue.insert(200)
         queue.insert(10)
@@ -73,7 +38,6 @@ class TestPriorityQueue(unittest.TestCase):
             queue.insert(element)
         max_element = queue.extract_max()
         self.assertEqual(max_element, array_max, msg=array)
->>>>>>> db73178322e3ad0ff49bd5c47a241994ecc0a1d5
 
 
 if __name__ == '__main__':
