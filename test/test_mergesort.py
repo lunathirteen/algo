04fb43b6349060f4685d3a71c8e59ba1b079cd95
Roman Lunev <r.v.lunev@gmail.com>
2021-06-16 22:49:59 +0300

    made test parametrized

--- test/test_mergesort.py
+++ test/test_mergesort.py
@@ -1,38 +1,24 @@
-
+import pytest
 from merge_sort import merge_sort, find_invariants
 
-def test_merge_sort_1():
+@pytest.mark.parametrize('array', [
+    ([2, 3, 9, 2, 9]),
+    ([1]),
+    ([]),
+    ([5,5,5,5,5]),
+    ([3000, 1500, 500, 100, 4, 1])
+])
+def test_merge_sort(array):
     print("test mergesort")
-    array = [2, 3, 9, 2, 9]
     sorted_array = merge_sort(array)
     assert sorted_array == sorted(array)
 
-def test_merge_sort_2():
-    print("test mergesort")
-    array = [1]
-    sorted_array = merge_sort(array)
-    assert sorted_array == sorted(array)
 
-def test_merge_sort_3():
-    print("test mergesort")
-    array = []
-    sorted_array = merge_sort(array)
-    assert sorted_array == sorted(array)
-
-def test_merge_sort_4():
-    print("test mergesort")
-    array = [5,5,5,5,5]
-    sorted_array = merge_sort(array)
-    assert sorted_array == sorted(array)
-
-def test_merge_sort_5():
-    print("test mergesort")
-    array = [3000, 1500, 500, 100, 4, 1]
-    sorted_array = merge_sort(array)
-    assert sorted_array == sorted(array)
-
-def test_find_invariants_1():
+@pytest.mark.parametrize('array, invariants', [
+    ([2, 3, 9, 2, 9], 2),
+])
+def test_find_invariants(array, invariants):
     print("test invariants")
-    array = [2, 3, 9, 2, 9]
+    
     inv = find_invariants(array)
-    assert inv == 2
+    assert inv == invariants
