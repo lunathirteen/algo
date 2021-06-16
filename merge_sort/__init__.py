914f458f7c697985498755664f60767b62791683
Roman Lunev <r.v.lunev@gmail.com>
2021-06-17 00:16:26 +0300

    refactored merge_sort

--- merge_sort/__init__.py
+++ merge_sort/__init__.py
@@ -2,41 +2,84 @@
 Первая строка содержит число 1≤n≤10^5,
 вторая — массив A[1…n], содержащий натуральные числа, не превосходящие 10^9.
 Необходимо посчитать число пар индексов 1≤i<j≤n, для которых A[i]>A[j].
-(Такая пара элементов называется инверсией массива. 
+Такая пара элементов называется инверсией массива
 Количество инверсий в массиве является в некотором смысле его мерой неупорядоченности:
-например, в упорядоченном по неубыванию массиве инверсий нет вообще, 
+например, в упорядоченном по неубыванию массиве инверсий нет вообще,
 а в массиве, упорядоченном по убыванию, инверсию образуют каждые два элемента.)
 """
-
-def _merge(a, b):
-    sorted_array = []
-    i = 0
-    j = 0
-    while i < len(a) and j < len(b):
-        if a[i] <= b[j]:
-            sorted_array.append(a[i])
-            i += 1
-        else:
-            sorted_array.append(b[j])
-            j += 1
-
-    if i >= len(a):
-        sorted_array += b[j:]
-    elif j >= len(b):
-        sorted_array += a[i:]
-
-    return sorted_array
 
 
 def merge_sort(array):
     if len(array) > 1:
-        m = (len(array))//2
-        l_array = merge_sort(array[:m])
-        r_array = merge_sort(array[m:])
-        return _merge(l_array, r_array)
-    else:
-        return array
+        m = (len(array)) // 2
+
+        l_array = array[:m]
+        r_array = array[m:]
+        merge_sort(l_array)
+        merge_sort(r_array)
+        i = 0
+        j = 0
+        k = 0
+        while i < len(l_array) and j < len(r_array):
+            if l_array[i] <= r_array[j]:
+                array[k] = l_array[i]
+                i += 1
+            else:
+                array[k] = r_array[j]
+                j += 1
+            k += 1
+
+        if i >= len(l_array):
+            array[k:] = r_array[j:]
+        elif j >= len(r_array):
+            array[k:] = l_array[i:]
+
+    return array
 
 
-def find_invariants(array):
-    pass
+def _merge(l_array, r_array):
+    sorted_array = []
+    i = 0
+    j = 0
+    while i < len(l_array) and j < len(r_array):
+        if l_array[i] <= r_array[j]:
+            sorted_array.append(l_array[i])
+            i += 1
+        else:
+            sorted_array.append(r_array[j])
+            j += 1
+
+    if i >= len(l_array):
+        sorted_array += r_array[j:]
+    elif j >= len(r_array):
+        sorted_array += l_array[i:]
+
+    return sorted_array
+
+
+def merge_sort_inv(array, invariants=0):
+
+    if len(array) > 1:
+        m = (len(array)) // 2
+
+        l_array = array[:m]
+        r_array = array[m:]
+        invariants += merge_sort_inv(l_array, invariants)
+        invariants += merge_sort_inv(r_array, invariants)
+        i = 0
+        j = 0
+        k = 0
+        while i < len(l_array) and j < len(r_array):
+            if l_array[i] <= r_array[j]:
+                array[k] = l_array[i]
+                i += 1
+            else:
+                array[k] = r_array[j]
+                invariants += 1
+                j += 1
+            k += 1
+
+        if i >= len(l_array):
+            array[k:] = r_array[j:]
+        elif j >= len(r_array):
+            array[k:] = l_array[i:]
